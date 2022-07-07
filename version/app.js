0d53dddcbc14ed39f44e12950616d261cd122cf0
Nithyashree <nithyashreekm99@gmail.com>
2022-07-07 14:52:15 +0530

    added version 3

--- version/app.js
+++ version/app.js
@@ -20,6 +20,14 @@ app.get('/map', function (req, res) {
   res.send(response);
 
 });
+app.get('/a1', function (req, res) {
+
+  response = 'This is version 3 of the app.' + '\n';
+
+  //send the response to the client
+  res.send(response);
+
+});
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
